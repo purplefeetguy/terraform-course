fe6d1f0cea5fb9ab4a9dc54c2d5be0c3d6fa11ef
purplefeetguy <purplefeetguy@hotmail.com>
2021-11-07 14:03:30 -1000

    broke out webapp and sql into separate tf files

---
+++ 05_Web_Sql_database/sqldb.tf
@@ -0,0 +1,15 @@
+resource "azurerm_sql_server" "sqldb" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_sql_database" "db" {
+  name                = "terraform-sqldatabase20"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sqldb.name
+}
