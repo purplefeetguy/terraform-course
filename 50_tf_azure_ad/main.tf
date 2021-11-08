660796a27b9343c412f2957f0140f286f5eac778
purplefeetguy <purplefeetguy@hotmail.com>
2021-11-07 16:26:44 -1000

    completed section 50 azure ad

--- 50_tf_azure_ad/main.tf
+++ 50_tf_azure_ad/main.tf
@@ -10,15 +10,21 @@ terraform {
 provider "azuread" {
 }
 
+# resource "azuread_user" "user" {
+#   user_principal_name = "houssem@houssemdellai.onmicrosoft.com"
+#   display_name        = "Houssem Dellai"
+#   mail_nickname       = "Houssem"
+#   password            = "SecretP@sswd99!"
+# }
+
 resource "azuread_user" "user" {
-  user_principal_name = "houssem@houssemdellai.onmicrosoft.com"
-  display_name        = "Houssem Dellai"
-  mail_nickname       = "Houssem"
+  user_principal_name = "jcastillo@purplefeetguy.onmicrosoft.com"
+  display_name        = "Jose Castillo"
+  mail_nickname       = "jose"
   password            = "SecretP@sswd99!"
 }
-
 resource "azuread_group" "group" {
-  name    = "MyTerraformGroup"
+  name    = "TFTCourseGroup"
   members = [
     azuread_user.user.object_id,
     /* more users */
