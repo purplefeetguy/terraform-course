a5b396a6227b71c7eeb128e1cec4048867867c7b
purplefeetguy <purplefeetguy@hotmail.com>
2021-11-07 18:26:25 -1000

    updated backend file for section 90 on terraform importing

---
+++ 90_import_resource/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-course-rg"
+    storage_account_name = "terraformcourse"
+    container_name       = "terraformcoursestorcontpfg"
+    key                  = "terraform-import.tfstate"
+  }
+}
